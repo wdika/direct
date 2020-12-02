50af4955b6993a386d973b431b1d87ca810a653e
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-02 11:46:41 +0100

    Update TECFIDERA

--- projects/tecfidera/preprocess_data.py
+++ projects/tecfidera/preprocess_data.py
@@ -24,13 +24,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def save_png_outputs(idx):
+def save_png_outputs(data, idx):
     # for i in tqdm(range(data.shape[0])):
     #     plt.imshow(data[i], cmap='gray')
     #     plt.savefig(output_dir + str(i) + '.png')
     #     plt.close()
 
-    plt.imshow(idx, cmap='gray')
+    plt.imshow(data[idx], cmap='gray')
     plt.savefig(str(idx) + '.png')
     plt.close()
 
@@ -47,12 +47,12 @@ def preprocess_vol(kspace, output_dir, num_workers=32):
 
     axial_imspace = torch.fft.ifftn(kspace.rename(None), dim=(0, 1, 2), norm="ortho")
     # axial_imspace = np.fft.ifftn(T.tensor_to_complex_numpy(kspace), axes=(0, 1, 2))
-    axial_target = np.abs(T.root_sum_of_squares(axial_imspace.refine_names('slice', 'height', 'width', 'coil', 'complex')).detach().cpu().numpy())
+    data = np.abs(T.root_sum_of_squares(axial_imspace.refine_names('slice', 'height', 'width', 'coil', 'complex')).detach().cpu().numpy())
 
     with multiprocessing.Pool(num_workers) as pool:
         os.chdir(output_dir)
         # pool.map(save_png_outputs(axial_target, output_dir=output_dir + '/axial/'),  range(len(axial_target)))
-        pool.map(save_png_outputs, tqdm(range(len(axial_target))))
+        pool.map(save_png_outputs, tqdm(data, range(len(data))))
 
     # logger.info("Processing the transversal plane...")
     # transversal_imspace = np.fft.ifftshift(np.fft.ifftn(np.transpose(kspace, (1, 0, 2, 3)), axes=(0, 1, 2)), axes=1)
