aa3a17e1006ad37a00adc7ab69b7ae8eeca45fa1
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-02 11:14:06 +0100

    Update TECFIDERA

--- projects/tecfidera/preprocess_data.py
+++ projects/tecfidera/preprocess_data.py
@@ -57,8 +57,8 @@ def preprocess_vol(kspace, output_dir):
     # axial_target = np.sqrt(np.sum(axial_imspace ** 2, -1))
     # del axial_imspace
 
-    # axial_imspace = torch.fft.ifftn(kspace.rename(None), dim=(0, 1, 2), norm="ortho")
-    axial_imspace = np.fft.ifftn(T.tensor_to_complex_numpy(kspace), axes=(0, 1, 2))
+    axial_imspace = torch.fft.ifftn(kspace.rename(None), dim=(0, 1, 2), norm="ortho")
+    # axial_imspace = np.fft.ifftn(T.tensor_to_complex_numpy(kspace), axes=(0, 1, 2))
     axial_target = T.root_sum_of_squares(axial_imspace.refine_names('slice', 'height', 'width', 'coil', 'complex')).detach().cpu().numpy()
 
     for i in tqdm(range(axial_target.shape[0])):
