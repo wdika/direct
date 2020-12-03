692839f3ef47568a0a7acf1bfc95c277d2f6695d
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-03 10:18:03 +0100

    Update TECFIDERA preprocessing

--- projects/tecfidera/preprocessing/preprocess_data.py
+++ projects/tecfidera/preprocessing/preprocess_data.py
@@ -7,96 +7,15 @@ import logging
 import sys
 import time
 from multiprocessing import Process
-from pathlib import Path
 
-import matplotlib.pyplot as plt
-import torch
-from torch.fft import ifftn
 from tqdm import tqdm
 
-from direct.data import transforms as T
-from projects.tecfidera.utils import readcfl
+from projects.tecfidera.preprocessing.utils import *
 
 # import h5py
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-def create_dir(path):
-    Path(path).mkdir(parents=True, exist_ok=True)
-
-
-def complex_tensor_to_real_np(x):
-    return torch.abs(x).detach().cpu().numpy()
-
-
-def save_png_outputs(data, output_dir):
-    create_dir(output_dir)
-
-    for i in range(data.shape[0]):
-        plt.imshow(data[i], cmap='gray')
-        plt.savefig(output_dir + str(i) + '.png')
-        plt.close()
-
-
-def preprocessing_ifft(kspace):
-    """
-
-    Parameters
-    ----------
-    kspace : torch.Tensor
-
-    Returns
-    -------
-    image space tensor of the axial plane transformed with the correct/fixed preprocessing steps
-    """
-    return T.fftshift(ifftn(kspace, dim=(0, 1, 2), norm="ortho"), dim=0)
-
-
-def extract_mask(kspace):
-    """
-
-    Parameters
-    ----------
-    kspace : torch.Tensor
-
-    Returns
-    -------
-    extracts the mask from the subsampled kspace, after summing the slice and the coil dimensions
-    """
-    return torch.where(torch.sum(torch.sum(torch.abs(kspace), 0), -1) > 0, 1, 0)
-
-
-def sense_reconstruction(imspace, csm, dim=-1):
-    """
-
-    Parameters
-    ----------
-    imspace : torch.Tensor
-    csm : torch.Tensor
-    dim : coil dimension
-
-    Returns
-    -------
-    reconstructed complex-valued image using SENSE
-    """
-    return torch.sum(imspace * torch.conj(csm), dim=dim)
-
-
-def csm_sense_coil_combination(csm, dim=-1):
-    """
-
-    Parameters
-    ----------
-    csm : torch.Tensor
-    dim : coil dimension
-
-    Returns
-    -------
-    coil combined image
-    """
-    return torch.sum(torch.conj(csm), dim=dim)
 
 
 def preprocessing(root, output, export_type, device):
---
+++ projects/tecfidera/preprocessing/utils.py
@@ -0,0 +1,107 @@
+# encoding: utf-8
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch.fft import ifftn
+
+from direct.data import transforms as T
+
+
+def readcfl(cfl):
+    h = open(cfl + ".hdr", "r")
+    h.readline()  # skip
+    line = h.readline()
+    h.close()
+    dims = [int(i) for i in line.split()]
+
+    # remove singleton dimensions from the end
+    n = int(np.prod(dims))
+    dims_prod = np.cumprod(dims)
+    dims = dims[:np.searchsorted(dims_prod, n) + 1]
+
+    # load data and reshape into dims
+    d = open(cfl + ".cfl", "r")
+    a = np.fromfile(d, dtype=np.complex64, count=n)
+    d.close()
+    a = a.reshape(dims, order='F')  # column-major
+
+    return a
+
+
+def create_dir(path):
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+
+def complex_tensor_to_real_np(x):
+    return torch.abs(x).detach().cpu().numpy()
+
+
+def save_png_outputs(data, output_dir):
+    create_dir(output_dir)
+
+    for i in range(data.shape[0]):
+        plt.imshow(data[i], cmap='gray')
+        plt.savefig(output_dir + str(i) + '.png')
+        plt.close()
+
+
+def preprocessing_ifft(kspace):
+    """
+
+    Parameters
+    ----------
+    kspace : torch.Tensor
+
+    Returns
+    -------
+    image space tensor of the axial plane transformed with the correct/fixed preprocessing steps
+    """
+    return T.fftshift(ifftn(kspace, dim=(0, 1, 2), norm="ortho"), dim=0)
+
+
+def extract_mask(kspace):
+    """
+
+    Parameters
+    ----------
+    kspace : torch.Tensor
+
+    Returns
+    -------
+    extracts the mask from the subsampled kspace, after summing the slice and the coil dimensions
+    """
+    return torch.where(torch.sum(torch.sum(torch.abs(kspace), 0), -1) > 0, 1, 0)
+
+
+def sense_reconstruction(imspace, csm, dim=-1):
+    """
+
+    Parameters
+    ----------
+    imspace : torch.Tensor
+    csm : torch.Tensor
+    dim : coil dimension
+
+    Returns
+    -------
+    reconstructed complex-valued image using SENSE
+    """
+    return torch.sum(imspace * torch.conj(csm), dim=dim)
+
+
+def csm_sense_coil_combination(csm, dim=-1):
+    """
+
+    Parameters
+    ----------
+    csm : torch.Tensor
+    dim : coil dimension
+
+    Returns
+    -------
+    coil combined image
+    """
+    return torch.sum(torch.conj(csm), dim=dim)
