36053956d7ec834d4ea97a85037c3a443ece9ea1
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-04 11:33:59 +0100

    load tecfidera masks

--- projects/tecfidera/dataset.py
+++ projects/tecfidera/dataset.py
@@ -41,6 +41,7 @@ class TECFIDERADataset(H5SliceData):
 
     def __getitem__(self, idx: int) -> Dict[str, Any]:
         sample = super().__getitem__(idx)
+        print(sample)
 
         if self.pass_mask:
             # mask should be shape (1, h, w, 1) mask provided is only w
