b051efcdbfdd4b530d21e99d47a312b176fcbaea
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-06 17:02:42 +0100

    check norm tecfidera

--- projects/tecfidera/preprocessing/preprocess_data.py
+++ projects/tecfidera/preprocessing/preprocess_data.py
@@ -65,7 +65,8 @@ def preprocessing(root, output, export_type, device):
                     csm = slice_selection(torch.from_numpy(readcfl(csm)).to(device), start=start, end=end)
                     del input_imspace
 
-                    print(np.max(complex_tensor_to_complex_np(imspace)), np.min(complex_tensor_to_complex_np(imspace)), np.max(complex_tensor_to_complex_np(csm)), np.min(complex_tensor_to_complex_np(csm)))
+                    print(np.max(complex_tensor_to_real_np(imspace)), np.min(complex_tensor_to_real_np(imspace)),
+                          np.max(complex_tensor_to_real_np(csm)), np.min(complex_tensor_to_real_np(csm)))
 
                     if export_type == 'png':
                         output_dir = output + '/png/' + subject.split('/')[-2] + '/' + acquisition.split('/')[
