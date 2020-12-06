f535b2404c7674b8876030f1b47adef8d01afc91
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-06 13:53:51 +0100

    do inference tecfidera

--- direct/data/mri_transforms.py
+++ direct/data/mri_transforms.py
@@ -219,6 +219,13 @@ class CropAndMask(DirectClass):
         backprojected_masked_kspace = backprojected_masked_kspace * T.conjugate(sample["sensitivity_map"])
         backprojected_masked_kspace = torch.sum(backprojected_masked_kspace, dim="coil")
         backprojected_masked_kspace = T.tensor_to_complex_numpy(backprojected_masked_kspace)
+        plt.subplot(1, 4, 1)
+        plt.imshow(np.abs(T.tensor_to_complex_numpy(sample["target"])), cmap='gray')
+        plt.subplot(1, 4, 2)
+        plt.imshow(np.abs(T.tensor_to_complex_numpy(torch.sum(T.conjugate(sample["sensitivity_map"]), dim="coil"))), cmap='gray')
+        plt.subplot(1, 4, 3)
+        plt.imshow(np.abs(sample["sampling_mask"].detach().cpu().numpy()), cmap='gray')
+        plt.subplot(1, 4, 4)
         plt.imshow(np.abs(backprojected_masked_kspace), cmap='gray')
         plt.show()
 
