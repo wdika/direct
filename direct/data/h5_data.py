3240c6ff01b2c43eb6fdbbe5217ca04bcf2bc36e
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-06 11:19:25 +0100

    do inference tecfidera

--- direct/data/h5_data.py
+++ direct/data/h5_data.py
@@ -204,7 +204,8 @@ class H5SliceData(DirectClass, Dataset):
         # If the sensitivity maps exist, load these
         if self.sensitivity_maps:
             sensitivity_map, _ = self.get_slice_data(
-                self.sensitivity_maps / filename.name, slice_no, key="sensitivity_map"
+                self.sensitivity_maps / filename.name, slice_no, key="sensitivity_map",
+                pass_attrs=self.pass_attrs, extra_keys=self.extra_keys
             )
             # sample["sensitivity_map"] = sensitivity_map
             sample.update({"sensitivity_map": sensitivity_map})
