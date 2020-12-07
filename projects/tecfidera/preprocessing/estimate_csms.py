e0067f80481dbe4d1a8d76c49b189cf0b4de1d51
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-07 11:18:25 +0100

    estimate 3D csms tecfidera

--- projects/tecfidera/preprocessing/estimate_csms.py
+++ projects/tecfidera/preprocessing/estimate_csms.py
@@ -57,7 +57,7 @@ def estimate_csms(root, output, calibration_region_size, export_type, device):
             csms = []
             for i in range(input_sense_ref_scan_kspace.shape[0]):
                 csms.append(bart(1, f"caldir {calibration_region_size}", input_sense_ref_scan_kspace[i]))
-            caldir_csm = np.concatenate(csms, 0)
+            caldir_csm = np.concatenate(csms, -2)
             print(caldir_csm.shape)
 
             # del input_sense_ref_scan_kspace
