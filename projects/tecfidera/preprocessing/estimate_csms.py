d2d012e78d3409b0b1f8740914e43795677c43c3
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-07 12:05:16 +0100

    estimate 3D csms tecfidera

--- projects/tecfidera/preprocessing/estimate_csms.py
+++ projects/tecfidera/preprocessing/estimate_csms.py
@@ -67,12 +67,14 @@ def estimate_csms(root, output, calibration_region_size, export_type, device):
             AXFLAIR_kspace = torch.from_numpy(readcfl(time_point + '/301_kspace'))
 
             print(AXFLAIR_kspace.shape, csm.shape)
+
+            pad = (1, AXFLAIR_kspace.shape[0,:,0,0] // 2, 2, AXFLAIR_kspace.shape[0,0,:,0] // 2)
+
             slices = []
             for slice in range(csm.shape[0]):
                 coils = []
                 for coil in range(csm.shape[-1]):
-                    coils.append(torch.nn.functional.pad(csm[slice,:,:,coil],
-                                                        AXFLAIR_kspace.shape[0,:,:,0], mode='constant', value=0))
+                    coils.append(torch.nn.functional.pad(csm[slice,:,:,coil], pad, mode='constant', value=0))
                 slices.append(torch.cat(coils, -1))
             AXFLAIR_csm = torch.cat(slices, 0)
             print(AXFLAIR_kspace.shape, AXFLAIR_csm.shape)
