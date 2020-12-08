fa54329fd2fe5b777cd29ff7a6a2a80e297ffd9a
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-08 16:28:39 +0100

    add tecfidera pics

--- projects/tecfidera/reconstruction/pics.py
+++ projects/tecfidera/reconstruction/pics.py
@@ -65,8 +65,8 @@ def pics_recon(data, device, reg=0.01):
     Run Parallel Imaging Compressed Sensing algorithm using the BART toolkit.
     """
     for i in range(len(data)):
-        masked_kspace = data['masked_kspace'][i]
-        sensitivity_map = data['sensitivity_map'][i]
+        masked_kspace = data[i]['masked_kspace']
+        sensitivity_map = data[i]['sensitivity_map']
 
         kspace = complex_tensor_to_complex_np(torch.from_numpy(masked_kspace).permute(1, 2, 0).unsqueeze(0))
         sense = complex_tensor_to_complex_np(fftshift(torch.from_numpy(sensitivity_map), dim=(1, 2)).permute(1, 2, 0).unsqueeze(0))
