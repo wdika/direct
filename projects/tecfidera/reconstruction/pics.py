c55433398814a8502d1c3e7b049fb3947ad6def0
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-08 14:14:23 +0100

    add tecfidera dataset

--- projects/tecfidera/reconstruction/pics.py
+++ projects/tecfidera/reconstruction/pics.py
@@ -64,7 +64,7 @@ def compute_pics_recon(masked_kspace, sensitivity_map, reg=0.01):
     """
     Run Parallel Imaging Compressed Sensing algorithm using the BART toolkit.
     """
-    sensitivity_map = normalize(sensitivity_map)
+    # sensitivity_map = normalize(sensitivity_map)
     print('sensitivity_map', np.max(np.abs(sensitivity_map)), np.min(np.abs(sensitivity_map)))
 
     kspace = complex_tensor_to_complex_np(fftshift(torch.from_numpy(masked_kspace).permute(1, 2, 0).unsqueeze(-2), dim=(0, 1)))
@@ -77,7 +77,7 @@ def compute_pics_recon(masked_kspace, sensitivity_map, reg=0.01):
     if plot:
         import matplotlib.pyplot as plt
         target = normalize(np.sum(sensitivity_map.conj() * np.fft.ifftn(masked_kspace, axes=(1, 2)), 0))
-        sense = np.sum(sensitivity_map.conj(), 0)
+        sense = normalize(np.sum(sensitivity_map.conj(), 0))
 
         print('target', np.max(np.abs(target)), np.min(np.abs(target)), np.max(np.abs(pred)), np.min(np.abs(pred)))
 
