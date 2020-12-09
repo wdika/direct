90828daf95989bcba45f033d7abf23df3e728039
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-09 14:08:58 +0100

    tecfidera preprocessing

--- projects/tecfidera/preprocessing/preprocess_data.py
+++ projects/tecfidera/preprocessing/preprocess_data.py
@@ -60,11 +60,14 @@ def preprocessing(root, output, skip_csm, export_type, device):
                     # input_imspace = preprocessing_ifft(input_kspace)
                     # input_imspace = complex_tensor_to_complex_np(input_imspace)
 
-                    print('input_kspace ', np.max(np.abs(input_kspace)), np.min(np.abs(input_kspace)))
-                    input_imspace = np.fft.ifftn(input_kspace, axes=(0, 1, 2))
-                    print('input_imspace 1 ', np.max(np.abs(input_imspace)), np.min(np.abs(input_imspace)))
-                    input_imspace = normalize(input_imspace)
-                    print('input_imspace normalized', np.max(np.abs(input_imspace)), np.min(np.abs(input_imspace)))
+                    np_input_imspace = normalize(np.fft.ifftn(input_kspace, axes=(0, 1, 2)))
+                    print('input_imspace numpy normalized', np.max(np.abs(np_input_imspace)),
+                          np.min(np.abs(np_input_imspace)))
+
+                    torch_input_imspace = normalize(ifftn(torch.from_numpy(input_kspace), dim=(0, 1, 2)))
+                    print('input_imspace torch normalized', torch.max(torch.abs(torch_input_imspace))[0],
+                          torch.min(torch.abs(torch_input_imspace))[0])
+
 
                     # Normalize data
                     # imspace = torch.from_numpy(normalize(complex_tensor_to_complex_np(input_imspace)))
