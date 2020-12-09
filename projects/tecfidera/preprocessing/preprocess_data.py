67ea37b7ecf1f78b18633f308beb57f14f7a9a38
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-09 09:48:12 +0100

    tecfidera preprocessing

--- projects/tecfidera/preprocessing/preprocess_data.py
+++ projects/tecfidera/preprocessing/preprocess_data.py
@@ -56,8 +56,7 @@ def preprocessing(root, output, skip_csm, export_type, device):
                     input_imspace = complex_tensor_to_complex_np(preprocessing_ifft(input_kspace))
 
                     # Normalize data
-                    # imspace = torch.from_numpy(normalize(input_imspace))
-                    imspace = torch.from_numpy(input_imspace)
+                    imspace = torch.from_numpy(normalize(input_imspace))
                     del input_imspace
 
                     imspace = slice_selection(imspace, start=start, end=end)
@@ -69,7 +68,7 @@ def preprocessing(root, output, skip_csm, export_type, device):
                         # TODO (dk, kp) : remove this normalization when saving to .cfl, then this line should go.
                         # input_csm = input_csm * np.expand_dims(np.sqrt(np.sum(input_csm.conj() * input_csm, -1)), -1)
 
-                        csm = torch.from_numpy(input_csm)
+                        csm = torch.from_numpy(normalize(input_csm))
                         csm = slice_selection(csm, start=start, end=end)
                         del input_csm
 
@@ -107,9 +106,7 @@ def preprocessing(root, output, skip_csm, export_type, device):
                         # TODO (dk) : find the correct transformation in pytorch,
                         #  so the norm doesn't change the scale of the data.
                         #  For now I will be using numpy, but that's inefficient.
-
                         kspace = complex_tensor_to_complex_np(fftn(imspace, dim=(1, 2), norm="ortho"))
-                        # kspace = np.fft.fftn(complex_tensor_to_complex_np(imspace), axes=(1, 2))
                         Process(target=save_h5_outputs, args=(kspace, "kspace", output_dir + name)).start()
 
                         if not skip_csm:
