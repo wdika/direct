35ed77e938f7c9d03c7ffc309e1046f9b0572c50
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-09 16:52:55 +0100

    tecfidera pics

--- projects/tecfidera/reconstruction/pics.py
+++ projects/tecfidera/reconstruction/pics.py
@@ -29,7 +29,7 @@ class DataTransform:
     def __call__(self, sample):
         # masked_kspace = complex_tensor_to_complex_np(torch.from_numpy(sample["kspace"]).permute(1, 2, 0).unsqueeze(0))
         # TODO : Find out why normalization works here but not when saving to h5 using the preprocess_data script.
-        masked_kspace = complex_tensor_to_complex_np(fftn(torch.from_numpy(normalize_rss(complex_tensor_to_complex_np(
+        masked_kspace = complex_tensor_to_complex_np(fftn(torch.from_numpy(normalize(complex_tensor_to_complex_np(
             ifftn(torch.from_numpy(sample["kspace"]).permute(1, 2, 0).unsqueeze(0), dim=(1, 2))))), dim=(1, 2)))
 
         sensitivity_map = complex_tensor_to_complex_np(
