1a0110bc7c34a803c0709ca05613f1d80416113e
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2020-12-09 15:35:02 +0100

    tecfidera pics

--- projects/tecfidera/reconstruction/pics.py
+++ projects/tecfidera/reconstruction/pics.py
@@ -3,6 +3,7 @@ __author__ = 'Dimitrios Karkalousos'
 
 import argparse
 import logging
+import multiprocessing
 import os
 import random
 import sys
@@ -31,8 +32,10 @@ class DataTransform:
         pass
 
     def __call__(self, sample):
-        masked_kspace = T.tensor_to_complex_numpy(T.to_tensor(sample["kspace"]).rename(None).permute(1, 2, 0, 3).unsqueeze(0))
-        sensitivity_map = T.tensor_to_complex_numpy(T.to_tensor(sample["sensitivity_map"]).rename(None).permute(1, 2, 0, 3).unsqueeze(0))
+        masked_kspace = T.tensor_to_complex_numpy(
+            T.to_tensor(sample["kspace"]).rename(None).permute(1, 2, 0, 3).unsqueeze(0))
+        sensitivity_map = T.tensor_to_complex_numpy(
+            T.to_tensor(sample["sensitivity_map"]).rename(None).permute(1, 2, 0, 3).unsqueeze(0))
 
         return masked_kspace, sensitivity_map, sample["filename"], sample["slice_no"]
 
@@ -56,11 +59,77 @@ def save_outputs(outputs, output_path):
             f["pics"] = pics_recon
 
 
-def pics_recon(data, device, reg=0.01):
-    """
-    Run Parallel Imaging Compressed Sensing algorithm using the BART toolkit.
-    """
-    kspace, sensitivity_map, filename, slice_no = data
+# def pics_recon(data, device, reg=0.01):
+#     """
+#     Run Parallel Imaging Compressed Sensing algorithm using the BART toolkit.
+#     """
+#     kspace, sensitivity_map, filename, slice_no = data
+#
+#     for i in range(20, kspace.shape[0]):
+#
+#         from torch.fft import ifftn
+#         from projects.tecfidera.preprocessing.utils import complex_tensor_to_complex_np
+#         imspace = complex_tensor_to_complex_np(ifftn(torch.from_numpy(kspace), dim=(1, 2)))
+#         print('imspace', np.max(np.abs(imspace)), np.min(np.abs(imspace)))
+#         print('sensitivity_map', np.max(np.abs(sensitivity_map)), np.min(np.abs(sensitivity_map)))
+#
+#         pred = bart(1, f'pics -g -i 200 -S -l1 -r {reg}',
+#                     complex_tensor_to_complex_np(torch.from_numpy(kspace).permute(1, 2, 0).unsqueeze(0)),
+#                     complex_tensor_to_complex_np(
+#                         ifftshift(torch.from_numpy(sensitivity_map).permute(1, 2, 0), dim=(0, 1)).unsqueeze(0))
+#                     )[0]
+#
+#         pred = complex_tensor_to_complex_np(ifftshift(torch.from_numpy(pred), dim=(0, 1)))
+#
+#         print('pred', np.max(np.abs(pred)), np.min(np.abs(pred)))
+#
+#         plot = True
+#         if plot:
+#             import matplotlib.pyplot as plt
+#             # imspace = np.fft.ifftn(masked_kspace, axes=(1, 2))
+#             rss_target = np.sqrt(np.sum(imspace ** 2, 0))
+#             target = np.sum(sensitivity_map.conj() * imspace, 0)
+#             sense = np.sqrt(np.sum(sensitivity_map ** 2, 0))
+#
+#             plt.subplot(2, 4, 1)
+#             plt.imshow(np.abs(rss_target), cmap='gray')
+#             plt.title('rss_target')
+#             plt.colorbar()
+#             plt.subplot(2, 4, 2)
+#             plt.imshow(np.angle(rss_target), cmap='gray')
+#             plt.title('rss_target phase')
+#             plt.colorbar()
+#             plt.subplot(2, 4, 3)
+#             plt.imshow(np.abs(sense), cmap='gray')
+#             plt.title('sense')
+#             plt.colorbar()
+#             plt.subplot(2, 4, 4)
+#             plt.imshow(np.angle(sense), cmap='gray')
+#             plt.title('sense phase')
+#             plt.colorbar()
+#             plt.subplot(2, 4, 5)
+#             plt.imshow(np.abs(target), cmap='gray')
+#             plt.title('ifft(masked_kspace) * sense.conj()')
+#             plt.colorbar()
+#             plt.subplot(2, 4, 6)
+#             plt.imshow(np.angle(target), cmap='gray')
+#             plt.title('ifft(masked_kspace) * sense.conj() phase')
+#             plt.colorbar()
+#             plt.subplot(2, 4, 7)
+#             plt.imshow(np.abs(pred), cmap='gray')
+#             plt.title('pics')
+#             plt.colorbar()
+#             plt.subplot(2, 4, 8)
+#             plt.imshow(np.angle(pred), cmap='gray')
+#             plt.title('pics phase')
+#             plt.colorbar()
+#             plt.show()
+#
+#     return pred
+#
+
+def pics_recon(idx):
+    kspace, sensitivity_map, filename, slice_no = data[idx]
 
     for i in range(20, kspace.shape[0]):
 
@@ -70,7 +139,7 @@ def pics_recon(data, device, reg=0.01):
         print('imspace', np.max(np.abs(imspace)), np.min(np.abs(imspace)))
         print('sensitivity_map', np.max(np.abs(sensitivity_map)), np.min(np.abs(sensitivity_map)))
 
-        pred = bart(1, f'pics -g -i 200 -S -l1 -r {reg}',
+        pred = bart(1, f'pics -g -i 200 -S -l1 -r 0.01',
                     complex_tensor_to_complex_np(torch.from_numpy(kspace).permute(1, 2, 0).unsqueeze(0)),
                     complex_tensor_to_complex_np(
                         ifftshift(torch.from_numpy(sensitivity_map).permute(1, 2, 0), dim=(0, 1)).unsqueeze(0))
@@ -122,18 +191,16 @@ def pics_recon(data, device, reg=0.01):
             plt.colorbar()
             plt.show()
 
-    return pred
+    return filename, slice_no, pred
 
 
-def main(args):
-    start_time = time.perf_counter()
-    data = TECFIDERADataset(root=args.data_root, transform=DataTransform(), sensitivity_maps=args.sensitivity_maps_root)
-
-    outputs = pics_recon(data=data, device=args.device)
-
-    time_taken = time.perf_counter() - start_time
-    logging.info(f"Run Time = {time_taken:}s")
-    save_outputs(outputs, args.output_path)
+def main(num_workers):
+    with multiprocessing.Pool(num_workers) as pool:
+        start_time = time.perf_counter()
+        outputs = pool.map(pics_recon, range(len(data)))
+        time_taken = time.perf_counter() - start_time
+        logging.info(f"Run Time = {time_taken:}s")
+        save_outputs(outputs, args.output_path)
 
 
 if __name__ == "__main__":
@@ -150,4 +217,6 @@ if __name__ == "__main__":
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
-    main(args)
+    data = TECFIDERADataset(root=args.data_root, transform=DataTransform(), sensitivity_maps=args.sensitivity_maps_root)
+
+    main(args.num_workers)
