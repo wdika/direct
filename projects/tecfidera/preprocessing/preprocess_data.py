1581cc37679e3fbe034bb7cb294bdb3b69e662b1
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2021-01-19 07:44:40 +0100

    TECFIDERA: transpose 3rd dim to 1st

--- projects/tecfidera/preprocessing/preprocess_data.py
+++ projects/tecfidera/preprocessing/preprocess_data.py
@@ -69,6 +69,8 @@ def preprocessing(root, output, skip_csm, export_type, device):
                         # Normalize data
                         # TODO (dk, kp) : make sure about the csm normalization. Here it seems the csm is normalized.
 
+                        csm = csm.permute(2, 0, 1)
+
                     if export_type == 'png':
                         output_dir = output + '/png/' + subject.split('/')[-2] + '/' + acquisition.split('/')[
                             -2] + '/' + name
