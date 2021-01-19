b7a0c0c088ac2f3c15e905e41fa11b520abcb599
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2021-01-19 10:20:36 +0100

    TECFIDERA: PICS recon

--- projects/tecfidera/reconstruction/pics.py
+++ projects/tecfidera/reconstruction/pics.py
@@ -31,7 +31,7 @@ class DataTransform:
         # sensitivity_map = complex_tensor_to_complex_np(
         #     ifftshift(torch.from_numpy(sample["sensitivity_map"]).permute(1, 2, 0).unsqueeze(0), dim=(1, 2)))
 
-        masked_kspace = np.expand_dims(np.transpose(sample["kspace"], (1, 2, 0)), 0)
+        masked_kspace = np.expand_dims(np.transpose(sample["kspace"], (1, 2, 0)), 0) * 0.8
         sensitivity_map = np.expand_dims(np.transpose(np.fft.fftshift(sample["sensitivity_map"], axes=(1, 2)), (1, 2, 0)), 0)
 
         return masked_kspace, sensitivity_map, sample["filename"], sample["slice_no"]
