3e82a93512f2cc87bbed7f710967ec76635b68f2
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2021-01-19 08:29:52 +0100

    TECFIDERA: SENSE recon

--- projects/tecfidera/preprocessing/preprocess_data.py
+++ projects/tecfidera/preprocessing/preprocess_data.py
@@ -59,7 +59,7 @@ def preprocessing(root, output, skip_csm, export_type, device):
                     imspace = ifftn(input_kspace, dim=(1, 2), norm="ortho")
                     del input_kspace
 
-                    imspace = imspace / torch.max(imspace)
+                    imspace = imspace / torch.max(torch.abs(imspace))
 
                     print(np.min(complex_tensor_to_real_np(imspace)), np.max(complex_tensor_to_real_np(imspace)))
 
