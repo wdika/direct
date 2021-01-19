f5518ea7d8ef567969b5c151ddb3bf62e2855887
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2021-01-19 11:45:45 +0100

    TECFIDERA: PICS recon

--- projects/tecfidera/reconstruction/pics.py
+++ projects/tecfidera/reconstruction/pics.py
@@ -27,12 +27,12 @@ class DataTransform:
         pass
 
     def __call__(self, sample):
-        # masked_kspace = complex_tensor_to_complex_np(torch.from_numpy(sample["kspace"]).permute(1, 2, 0).unsqueeze(0))
-        # sensitivity_map = complex_tensor_to_complex_np(
-        #     ifftshift(torch.from_numpy(sample["sensitivity_map"]).permute(1, 2, 0).unsqueeze(0), dim=(1, 2)))
+        masked_kspace = complex_tensor_to_complex_np(torch.from_numpy(sample["kspace"]).permute(1, 2, 0).unsqueeze(0))
+        sensitivity_map = complex_tensor_to_complex_np(
+            ifftshift(torch.from_numpy(sample["sensitivity_map"]).permute(1, 2, 0).unsqueeze(0), dim=(1, 2)))
 
-        masked_kspace = np.expand_dims(np.transpose(sample["kspace"], (1, 2, 0)), 0)
-        sensitivity_map = np.expand_dims(np.transpose(np.fft.fftshift(sample["sensitivity_map"], axes=(1, 2)), (1, 2, 0)), 0)
+        # masked_kspace = np.expand_dims(np.transpose(sample["kspace"], (1, 2, 0)), 0)
+        # sensitivity_map = np.expand_dims(np.transpose(np.fft.fftshift(sample["sensitivity_map"], axes=(1, 2)), (1, 2, 0)), 0)
 
         return masked_kspace, sensitivity_map, sample["filename"], sample["slice_no"]
 
