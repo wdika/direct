c6def9c68234c42e200f60983eac9449956facc7
dimitriskarkalousos <dimitriskarkalousos@gmail.com>
2021-01-19 09:10:22 +0100

    TECFIDERA: PICS recon

--- projects/tecfidera/preprocessing/preprocess_data.py
+++ projects/tecfidera/preprocessing/preprocess_data.py
@@ -69,13 +69,12 @@ def preprocessing(root, output, skip_csm, export_type, device):
 
                     input_kspace = readcfl(filename_kspace.split('.')[0])
                     mask = np.where(np.sum(np.sum(np.abs(input_kspace), 0), -1) > 0, 1, 0)
-
-                    input_kspace = slice_selection(input_kspace, start=start, end=end)
                     imspace = np.fft.ifft2(input_kspace, axes=(1, 2))
                     imspace = imspace / np.max(np.abs(imspace))
 
                     if not skip_csm:
-                        csm = slice_selection(readcfl(filename_kspace.split('_')[0] + '_csm'), start=start, end=end)
+                        # csm = slice_selection(readcfl(filename_kspace.split('_')[0] + '_csm'), start=start, end=end)
+                        csm = readcfl(filename_kspace.split('_')[0] + '_csm')
 
                         # Normalize data
                         # TODO (dk, kp) : make sure about the csm normalization. Here it seems the csm is normalized.
