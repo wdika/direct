7169a41f6b80213e2e0c8f3d4469f13983609721
wdika <dimitriskarkalousos@gmail.com>
2021-01-25 16:31:46 +0100

    tests

--- projects/tecfidera/preprocessing/load_listdata.py
+++ projects/tecfidera/preprocessing/load_listdata.py
@@ -1,7 +1,15 @@
 # encoding: utf-8
 __author__ = 'Dimitrios Karkalousos'
 
+import argparse
+import logging
+import sys
+import time
+
 import numpy as np
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def load_kprops(list_file):
@@ -17,12 +25,12 @@ def load_kprops(list_file):
     """
     kprops = {}
     with open(list_file, 'r') as fid:
-        _kprops = (r.split()[1:] for r in fid if r[0] == '.')
-        for prop in _kprops:
+        for prop in (r.split()[1:] for r in fid if r[0] == '.'):
             if len(prop) in [8, 9]:
                 fieldname = '_'.join(prop[3:-2])
             else:
                 fieldname = '_'.join(prop[3].split('-'))
+
             if prop[-2] == ':':
                 try:
                     val = [int(prop[-1])]
@@ -31,7 +39,9 @@ def load_kprops(list_file):
             else:
                 val = list(map(int, prop[-2:]))
             kprops[fieldname] = val
+
         kprops['coil_channel_combination'] = list(map(int, prop[-1]))
+
     return kprops
 
 
@@ -78,6 +88,7 @@ def load_kspace(path):
     """
     with open(path + '.data', 'rb') as f:
         complexdata = np.fromfile(f, dtype='<f4')
+
     complexdata = complexdata[::2] + 1.0j * complexdata[1::2]
 
     kspace = get_acquisition_meta_data(path + '.list')
@@ -132,7 +143,27 @@ def get_kspace_from_listdata(fdir):
 
     for n, (y, z, m, t, a) in enumerate(zip(yy, zz, mm, tt, aa)):
         kspace_filled[:, y, z, :, m, t, a] = tmp[:, :, n]
+
         if np.fmod(n, np.around(len(yy) / 100)) == 0:
             print('Progress: {}%'.format(np.around((n / len(yy)) * 100)), end='\r')
 
-    return kspace_filled, None
+    return kspace_filled
+
+
+def main(args):
+    start_time = time.perf_counter()
+    logger.info("Saving data. This might take some time, please wait...")
+    get_kspace_from_listdata(args.root)
+    time_taken = time.perf_counter() - start_time
+    logger.info(f"Done! Run Time = {time_taken:}s")
+
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('root', type=str, help='Root dir containing folders with cfl files.')
+    return parser
+
+
+if __name__ == '__main__':
+    args = create_arg_parser().parse_args(sys.argv[1:])
+    main(args)
